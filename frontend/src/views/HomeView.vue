ac63f5455242b089bb403565710ba0f0162e051a
Symbat Kudabayeva <symbat.kudabayeva@kimep.kz>
2023-03-21 03:30:42 +0600

    21/03

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,32 +5,32 @@
     </div>
     <div class="menu-bar">
       <ul>
-          <li><a href="/mocks">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Services</a></li>
+          <li><a href="/?">Home</a></li>
+          <li><a href="#">My lesson plan</a></li>
+          <li><a href="#">Extra tasks</a></li>
           <li><a href="#">Contact</a></li>
       </ul>
     </div>
     <div class="menu-buttons">
-      <button>Log in</button>
-      <button>Register</button>
+     
+      <button><a href="/register">Register</a></button>
     </div>
     <div class="container">
       <form>
 
-        <h1 style="color: #2f1c6a; font-family: Arial, Helvetica, sans-serif, sans-serif; font-size: 36px!important; font-weight: 700!important; text-shadow: 2px 2px #ddd; 
-        letter-spacing: 2px; margin-bottom: 25px!important; margin-top: 25px!important; text-align: center!important;">Welcome to an IELTS tracker!</h1>
+        <h1 style="color: black; font-family: Garamond, sans-serif; font-size: 36px!important; font-weight: 700!important; text-shadow: 2px 2px #ddd; 
+        letter-spacing: 2px; margin-bottom: 50px!important; margin-top: 25px!important; text-align: center!important;">Log In</h1>
         
           
         <div class="form-group">
 
-          <svg class="input-icon" xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill= #00239c viewBox="0 0 16 16">
+          <svg class="input-icon" xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill= black viewBox="0 0 16 16">
           <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z"></path>
           </svg>
           <input type="text" placeholder="Email">
         </div>
         <div class="form-group">
-          <svg class="input-icon" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill= #00239c viewBox="0 0 16 16">
+          <svg class="input-icon" xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill= black viewBox="0 0 16 16">
           <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"></path>
           </svg>
           <input type="password" id="password-field" placeholder="Password">
@@ -40,11 +40,11 @@
           <a href="/mocks"><button>
           <span class="transition"></span>
           <span class="gradient"></span>
-          <span class="btn">Log In</span>
+          <span class="btn">Log in</span>
           </button></a>
         </div>
       </form>
-    <p style="font-family: Open Sans, sans-serif;"> Don't have an account? <a style= "font-family: Open Sans, sans-serif; font-weight: bold;" href="/register">Sign up!</a></p>
+    <p style="font-family: Garamond, serif; font-size: 28px;"> Don't have an account? <a style= "font-family: Garamond, serif; font-weight: bold; font-size: 28px;" href="/register">Sign up!</a></p>
     </div>
     </body>
   </template>
@@ -116,7 +116,8 @@
     .menu-bar ul
         list-style: none
         margin: 0
-        padding: 0   
+        padding: 0
+        text-decoration: none   
     .menu-bar li
         display: inline-block
         margin: 0 50px
@@ -127,22 +128,33 @@
         font-family: "Garamond", serif
         font-size: 28px
         font-weight: bold
+        text-decoration: none   
         
     
     .menu-buttons 
       flex: 0 0 auto
       display: flex
       align-items: center
+      text-decoration: none
+      
 
 
-    .menu-buttons button 
-      background-color: transparent
+    .menu-buttons button
+      background-color: black
       border: 2px solid black
-      color: black
-      font-size: 16px
+      color: white
+      font-size: 36px
       padding: 10px 20px
       margin-left: 20px
       cursor: pointer
+      font-family: "Garamond", serif
+      margin-top: -15px
+      font-weight: bold
+      text-decoration: none
+    .menu-buttons button a:visited
+      color: white
+      text-decoration: none
+      
     
 
 
@@ -152,7 +164,7 @@
     box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)
     padding: 40px
     width: 80%
-    max-width: 500px
+    max-width: 700px
     text-align: center
     transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1)
     display: inline-block
@@ -161,6 +173,7 @@
     top: 50%
     left: 50%
     transform: translate(-50%, -50%)
+    margin-top: 50px
 
   .container:hover
     box-shadow: 0 30px 38px rgba(0,35, 156, 0.3), 0 30px 12px rgba(0,35, 156, 0.22)
@@ -186,8 +199,8 @@
     border: 1px solid #dadce0
     border-radius: 4px
     box-shadow: 0 0 0 transparent
-    font-size: 16px
-    line-height: 24px
+    font-size: 28px
+    line-height: 30px
     outline: transparent
     padding: 11px 16px
     transition: border .3s,box-shadow .3s
@@ -202,16 +215,20 @@
 
   button
 
-    font-family: "Open Sans", sans-serif
-    font-size: 17px
-    padding: 1em 2.7em
+    font-family: "Garamond", serif
+    font-size: 36px
     font-weight: 500
     background: #1F2937
     color: white
     border: none
     position: relative
     overflow: hidden
-    border-radius: 0.6em
+    border-radius: 4px
+    padding: 11px 100px
+    button-height: 30px
+    margin-top: 50px
+    cursor: pointer
+    font-weight: bold
 
   .gradient
     position: absolute
@@ -219,18 +236,19 @@
     height: 100%
     left: 0
     top: 0
-    border-radius: 0.6em
+    border-radius: 4px
     margin-top: -0.25em
     background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.3))
 
   .btn
     position: relative
     top: -1px
+    
 
   .transition
     transition-timing-function: cubic-bezier(0, 0, 0.2, 1)
     transition-duration: 500ms
-    background-color: rgba(0,35, 156, 1.00)
+    background-color: rgba(0, 0, 0, 1.0)
     border-radius: 9999px
     width: 0
     height: 0
